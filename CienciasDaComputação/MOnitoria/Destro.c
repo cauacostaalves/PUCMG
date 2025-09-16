ec9eaddb2243a22802a1cc98aef7542bd5e8a176
Cauã Costa Alves <91996777+cauacostaalves@users.noreply.github.com>
2025-09-16 15:56:45 -0300

    reusmo ia

---
+++ CienciasDaComputação/MOnitoria/Destro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int idade=0, QC=0, A=0, QHD=0, IHD=0;
+    float media;
+    char mao, sexo;
+    
+    do{
+        printf("idade: " );
+        scanf("%d",&idade);
+        while(idade > 0 );
+    }
+    
+}
